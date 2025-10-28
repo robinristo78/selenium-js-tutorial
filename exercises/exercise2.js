3fdb69616b9b0217f985d0346c67ca7df27c34c1
robinristo78 <robin-risto.kivit@voco.ee>
2025-10-28 10:52:40 +0200

    exercise 2 fix

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -6,17 +6,18 @@ async function openWiki() {
         // Open Wikipedia
         await driver.get('https://www.wikipedia.org/');
 
-        // Type 'WebDriver' into the search box and press Enter
-        await driver.findElement(By.name('q')).sendKeys('WebDriver', Key.ENTER);
+        // Otsi otsingukasti ja sisesta "Selenium (software)" ning vajuta Enter
+        let searchBox = await driver.findElement(By.name('search'));
+        await searchBox.sendKeys('Selenium (software)', Key.ENTER);
 
+        // Oota, kuni leht laeb ja pealkiri muutub
+        await driver.wait(until.titleContains('Selenium'), 10000);
 
-        // Wait for the first search result to appear
-        let firstResult = await driver.wait(
-            until.elementLocated(By.css('h2 a')), 
-            10000
-        );
+        // Võta lehe pealkiri
+        let title = await driver.getTitle();
 
-        console.log("First result:", await firstResult.getText());
+        // Prindi pealkiri terminali
+        console.log("Lehe pealkiri:", title);
     } finally {
         // Close the browser
         await driver.quit();
